1daed8fc69d7892d36c2d5286ced8f6a094cd2dd
lcx2002123 <lcx2002123@gmail.com>
2017-12-26 18:08:24 +0900

    day11

--- www/handlers.py
+++ www/handlers.py
@@ -18,6 +18,20 @@ from config import configs
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
 
+def check_admin(request):
+    if request.__user__ is None or not request.__user__.admin:
+        raise APIPermissonError()
+
+def get_page_index(page_str):
+    p = 1
+    try:
+        p = int(page_str)
+    except ValueError as e:
+        pass
+    if p < 1:
+        p = 1
+    return p
+
 def user2cookie(user,max_age):
     '''
     Generate cookie str by user.
@@ -28,6 +42,10 @@ def user2cookie(user,max_age):
     L = [user.id,expires,hashlib.sha1(s.encode('utf-8')).hexdigest()]
     return '-'.join(L)
     
+def text2html(text):
+    lines = map(lambda s:'<p>%s</p>' % s.replace('&','&amp;').replace('<','&lt;').replace('>', '&gt;'),filter(lambda s: s.strip() != '',text.split('\n')))
+    return ''.join(lines)
+
 async def cookie2user(cookie_str):
     '''
     parse cookie and load user if cookie is valid.
@@ -53,7 +71,6 @@ async def cookie2user(cookie_str):
         logging.exception(e)
         return None
         
-        
 @get('/')
 def index(request):
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
@@ -71,6 +88,19 @@ def index(request):
         'blogs': blogs
     }
 
+@get('/blog/{id}')
+async def get_blog(id):
+    blog = await Blog.find(id)
+    comments = await Comment.findAll('blog_id=?',[id],orderBy='created_at desc')
+    for c in comments:
+        c.html_content = text2html(c.content)
+    blog.html_content = markdown2.markdown(blog.content)
+    return {
+        '__template__':'blog.html',
+        'blog':blog,
+        'comments':comments
+    }
+        
 @get('/register')
 def register():
     return {
@@ -83,23 +113,16 @@ def signin():
         '__template__': 'signin.html'
     }
 
-@get('/api/users')
-async def api_get_users():
-    users = await User.findAll(orderBy='created_at desc')
-    for u in users:
-        u.passwd = '******'
-    return dict(users=users)
-
 @post('/api/authenticate')
 async def authenticate(*,email,passwd):
     if not email:
         raise APIValueError('email','Invalid email.')
     if not passwd:
-        raise APIValueError('email','Email not exist.')
+        raise APIValueError('passwd','Invalid password.')
     users = await User.findAll('email=?',[email])
     if len(users) == 0:
-        raise APIValueError('passwd','Invalid password.')
-    user = user[0]
+        raise APIValueError('email','Email not exist.')
+    user = users[0]
     #check passwd:
     sha1 = hashlib.sha1()
     sha1.update(user.id.encode('utf-8'))
@@ -115,7 +138,6 @@ async def authenticate(*,email,passwd):
     user.passwd = '******'
     r.content_type = 'application/json'
     r.body = json.dumps(user,ensure_ascii=False).encode('utf-8')
-    users = await User.findAll(orderBy='created_at desc')
     return r
     
 @get('/signout')
@@ -126,23 +148,30 @@ def signout(request):
     logging.info('user signed out.')
     return r
     
+@get('/manage/blogs/create')
+def manage_create_blog():
+    return {
+        '__template__':'manage_create_edit.html',
+        'id':'',
+        'action':'/api/blogs'
+    }
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
 @post('/api/users')
 async def api_register_user(*,email,name,passwd):
-    if not name or name.strip():
+    if not name or not name.strip():
         raise APIValueError('name')
     if not email or not _RE_EMAIL.match(email):
         raise APIValueError('email')
-    if not passwd or not _RE.match(passwd):
+    if not passwd or not _RE_SHA1.match(passwd):
         raise APIValueError('passwd')
     users = await User.findAll('email=?',[email])
     if len(users) > 0:
         raise APIError('register:failed', 'email', 'Email is already in use.')
     uid = next_id()
     sha1_passwd = '%s:%s' % (uid,passwd)
-    user = User(id=uid,name=name.strip(),email=email,passwd=hashlib.sha1(sha1_passwd).encode('utf-8').hexdigest(),image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+    user = User(id=uid,name=name.strip(),email=email,passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
     await user.save()
     #make session cookie:
     r = web.Response()
@@ -151,3 +180,22 @@ async def api_register_user(*,email,name,passwd):
     r.content_type = 'application/json'
     r.body = json.dumps(user,ensure_ascii=False).encode('utf-8')
     return r
+
+@get('/api/blogs/{id}')
+async def api_get_blog(*,id):
+    blog = await Blog.find(id)
+    return blog
+    
+@post('/api/blogs')
+async def api_create_blog(request,*,name,summary,content):
+    check_admin(request)
+    if not name or not name.strip():
+        raise APIValueError('name','name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary','summary cannot be empty.')
+    if not content or not content.strip():
+        raise APIValueError('content','content cannot be empty.')
+    blog = Blog(user_id=request.__user__.id,user_name=request.__user__.name,user_image=request.__user__.image,name=name.strip(),summary=summary.strip(),content=content.strip())
+    await blog.save()
+    return blog
+    
